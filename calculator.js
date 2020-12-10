0765d8ba8cd5bfbb33fdc2aed77f6faf500d3995
Gary Arzumanyan <garyarzumanyan@gmail.com>
2020-12-09 23:50:32 -0800

    Cleaned up UI and added button and clear functionality so the numbers get displayed in the results area.

--- calculator.js
+++ calculator.js
@@ -27,3 +27,36 @@ function operate (operator, num1, num2){
     }
 }
 
+document.querySelectorAll('.button').forEach(button => button.addEventListener('click', displayButton));
+
+function displayButton(e){ 
+    const buttonId = e.target.id;
+    switch(buttonId){
+        case "num0":
+        case "num1":
+        case "num2":
+        case "num3":
+        case "num4":
+        case "num5":
+        case "num6":
+        case "num7":
+        case "num8":
+        case "num9": 
+        case "AC":  { displayResults(e.target.textContent)
+                        return e.target.textContent;
+                    }
+        
+    
+    } 
+   
+}
+
+function displayResults(textContents){
+    let resultNode = document.getElementById("result");
+    if(textContents === "AC") resultNode.textContent = "0"; 
+    else{
+        if(resultNode.textContent === "0") resultNode.textContent = "";
+        if(resultNode.textContent.toString().length <11) resultNode.textContent += textContents;
+        else resultNode.textContent = resultNode.textContent.slice(1)+textContents;
+    }
+   }
